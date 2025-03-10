ca3b09b19489575fe2e5c986ea3bc4cc6b7fc017
farling42 <github@amusingtime.uk>
2025-03-10 19:25:46 +0000

    New improved release action

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -20,7 +20,7 @@ esbuild.build({
 	external: ['obsidian', 'electron', ...builtins],
 	format: 'cjs',
 	watch: !prod,
-	target: 'es2016',
+	target: 'es2018',
 	logLevel: "info",
 	sourcemap: prod ? false : 'inline',
 	treeShaking: true,
